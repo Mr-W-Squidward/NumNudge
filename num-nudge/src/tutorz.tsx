9bd9e3b670dde356701fc52d408604e72cba786f
Wajeeh <alamwajeeh@gmail.com>
2024-10-06 21:16:50 -0400

    plz work

--- num-nudge/src/tutorz.tsx
+++ num-nudge/src/tutorz.tsx
@@ -1,38 +1,43 @@
 import React from "react";
 import TutorCard from "./TutorCard";
+import wajeehimg from "/src/assets/imgs/wajeeh.png";
+import alifimg from "/src/assets/imgs/alif.png";
+import raiyanimg from "/src/assets/imgs/raiyan.png";
+import sazidimg from "/src/assets/imgs/sazid.png";
+import arminimg from "/src/assets/imgs/armin.png";
 
 const Tutors: React.FC = () => {
   const tutors = [
     {
-      image: "/src/assets/imgs/wajeeh.png",
+      image: wajeehimg,
       name: 'Wajeeh Alam',
       grade: 'Grade 11 - IB',
       intro: 'CEO/Founder',
       blurb: 'I\'m Wajeeh! A dedicated programmer. I also teach math here and I created this site to appeal to those who need a cheap, reliable and fun tutor! I look forward to hearing your experience.'
     },
     {
-      image: "/src/assets/imgs/alif.jpg",
+      image: alifimg,
       name: 'Alif Ashraf',
       grade: 'Grade 12',
       intro: 'Mathematics Tutor',
       blurb: 'Hey I\'m Alif! One of NumNudges math tutors. It\'s a passion of mine to be able to provide affordable, reliable, and fun math tutoring for all ages for those who need it! I look forward to helping you on your math journey!'
     },
     {
-      image: "/src/assets/imgs/raiyan.png",
+      image: raiyanimg,
       name: 'Raiyan Khan',
       grade: 'Grade 12',
       intro: 'Python Tutor',
       blurb: 'Hello, I\'m Raiyan! I\'m a math/computer science tutor at NumNudge with the goal of showing others the beauty of learning! If you need a responsible, experienced, and affordable tutor that is ready to listen, look no further. Looking forward to accelerating your learning journey!'
     },
     {
-      image: "/src/assets/imgs/armin.jpg",
+      image: arminimg,
       name: 'Armin Farhad',
       grade: 'Grade 12',
       intro: 'Web Development Tutor',
       blurb: 'Hi, I\'m Armin. One of NumNudges tutors. I\'ve taught math, chemistry, physics and computer science for very cheap, helping those who struggle to understand material taught in classes.'
     },
     {
-      image: "/src/assets/imgs/sazid.jpg",
+      image: sazidimg,
       name: 'Sazid Malik',
       grade: 'Grade 12',
       intro: 'Mathematics Tutor',
